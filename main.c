f72e798e6eda0ef5946791096fe3e13a9325c3f4
mgombos <50267286+PumpkintheIII@users.noreply.github.com>
2023-02-14 13:41:35 +0000

    Iteration 1

--- main.c
+++ main.c
@@ -3,9 +3,20 @@
   Seconds to Minutes
   Max Gombos
 */
-#include <stdio.h>
+#include <stdio.h> //import input and output functions
 
-int main(void) {
+int main(void) { //define main function
+  float seconds, minutes; //intialize variable as float
+
+  printf("This program allows you to enter an amount of seconds and the program will convert them to minutes. You will get one prompt:\n\t- Seconds: Please enter your seconds here\n"); //instructions
+  
+  printf("Seconds: "); //ask user for input
+  scanf("%f", &seconds); //accept input and format as float
+
+  minutes = seconds/60; //convert seconds to minutes
+
+  //print output:
+  printf("%0.3f seconds is equal to %0.3f minutes.", seconds, minutes);
   
   return 0;
 }
